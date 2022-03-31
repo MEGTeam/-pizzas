10a4ca7d18e447940e645ce5d50ac370aadb88bf
Belal-2000 <belalma3arek#@gmail.com>
2022-03-31 14:20:17 +0200

    Finished Menu part

--- script/main.js
+++ script/main.js
@@ -1,7 +1,15 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import * as module from "./util.js";
 
-
+module.default.MYBUTTONS.forEach((e, i) => {
+    e.addEventListener("click", (ev) => {
+        module.default.removeActive(module.default.MYBUTTONS, "activeB");
+        e.classList.add("activeB");
+        module.default.removeActive(module.default.MYSECTIONS, "activeM");
+        module.default.MYSECTIONS[i].classList.add("activeM");
+    });
+});
